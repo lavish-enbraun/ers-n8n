7b28f094c17b62daf2168bcffc24bcc5d7ff8b48
Lavish <lavish.pareta@enbraun.com>
2025-11-14 10:54:02 +0530

    OAuth Configuration (#1)
    * Single Click Auth Configuration
    
    Co-authored-by: sarthak ahuja <sarthak.ahuja@enbraun.com>

--- credentials/ErsAppOAuth2Api.credentials.ts
+++ credentials/ErsAppOAuth2Api.credentials.ts
@@ -1,84 +1,83 @@
-import type { ICredentialType, INodeProperties, IconFile } from 'n8n-workflow';
+import type { 
+    ICredentialType, 
+    INodeProperties, 
+    IconFile,
+} from 'n8n-workflow';
 
 export class ErsAppOAuth2Api implements ICredentialType {
-	name = 'ersAppOAuth2Api';
+    name = 'ersAppOAuth2Api';
 
-	extends = ['oAuth2Api'];
+    extends = ['oAuth2Api'];
 
-	displayName = 'Ers App OAuth2 API';
+    // eslint-disable-next-line n8n-nodes-base/cred-class-field-display-name-missing-api, n8n-nodes-base/cred-class-field-display-name-missing-oauth2
+    displayName = 'Sign in with eResource Scheduler';
 
-	icon: { light: IconFile; dark: IconFile } = {
-		light: 'file:../nodes/ErsApp/ersApp.svg',
-		dark: 'file:../nodes/ErsApp/ersApp.dark.svg',
-	};
+    icon: { light: IconFile; dark: IconFile } = {
+        light: 'file:../nodes/ErsApp/ersApp.svg',
+        dark: 'file:../nodes/ErsApp/ersApp.dark.svg',
+    };
 
-	// Link to your community node's README
-	documentationUrl = 'https://github.com/org/-ers-app?tab=readme-ov-file#credentials';
+    // Link to your community node's README
+    documentationUrl = 'https://github.com/org/-ers-app?tab=readme-ov-file#credentials';
 
-	properties: INodeProperties[] = [
-		{
-			displayName: 'Client ID',
-			name: 'clientId',
-			type: 'hidden',
-			default: 'I7XLLz8dQ9P8wa0q',
-		},
-		{
-			displayName: 'Client Secret',
-			name: 'clientSecret',
-			type: 'hidden',
-			typeOptions: {
-				password: true,
-			},
-			required: true,
-			default: 'fixed-client-secret-abcde',
-		},
-		{
-			displayName: 'Grant Type',
-			name: 'grantType',
-			type: 'hidden',
-			default: 'authorizationCode',
-		},
-		{
-			displayName: 'Authorization URL',
-			name: 'authUrl',
-			type: 'hidden',
-			default: `http://192.168.1.16:8080/login/oauth/authorize`,
-		},
-		{
-			displayName: 'Access Token URL',
-			name: 'accessTokenUrl',
-			type: 'hidden',
-			default: `http://192.168.1.16:8080/login/oauth/token`,
-		},
-		{
-			displayName: 'Auth URI Query Parameters',
-			name: 'authQueryParameters',
-			type: 'hidden',
-			default: '',
-		},
-		{
-			displayName: 'Scope',
-			name: 'scope',
-			type: 'hidden',
-			default: 'users:read users:write companies:read',
-		},
-		{
-			displayName: 'Authentication',
-			name: 'authentication',
-			type: 'hidden',
-			default: 'header',
-		},
-		{
-			displayName: 'OAuth Redirect URL',
-			name: 'oauthRedirectUrl',
-			type: 'hidden',
-			default: 'http://localhost:5678/rest/oauth2-credential/callback',
-			displayOptions: {
-			  show: {
-				// never true
-				neverShow: ['true'],
-			  },
-			},
-		  }
-	];
+    properties: INodeProperties[] = [
+        {
+            displayName: 'Client ID',
+            name: 'clientId',
+            type: 'hidden',
+            default: '121',
+        },
+        {
+            displayName: 'Client Secret',
+            name: 'clientSecret',
+            type: 'hidden',
+            typeOptions: {
+                password: true,
+            },
+            required: true,
+            default: '122',
+        },
+        {
+            displayName: 'Grant Type',
+            name: 'grantType',
+            type: 'hidden',
+            default: 'authorizationCode',
+        },
+        {
+            displayName: 'Authorization URL',
+            name: 'authUrl',
+            type: 'hidden',
+            default: `http://192.168.1.16:8080/login/oauth/authorize`,
+        },
+        {
+            displayName: 'Access Token URL',
+            name: 'accessTokenUrl',
+            type: 'hidden',
+            default: `http://192.168.1.16:8080/login/oauth/token?client_source=system`,
+        },
+        {
+            displayName: 'Auth URI Query Parameters',
+            name: 'authQueryParameters',
+            type: 'hidden',
+            default: 'client_source=system',
+        },
+        {
+            displayName: 'Scope',
+            name: 'scope',
+            type: 'hidden',
+            default: 'users:read users:write companies:read',
+        },
+        {
+            displayName: 'Authentication',
+            name: 'authentication',
+            type: 'hidden',
+            default: 'header',
+        },
+        {
+            displayName: 'OAuth Redirect URL',
+            name: 'oauthRedirectUrl',
+            type: 'hidden',
+            default: 'http://localhost:5678/rest/oauth2-credential/callback',
+        },
+    ];
 }
