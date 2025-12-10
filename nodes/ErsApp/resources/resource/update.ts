58d7f83da6bf375e601f9e81a2b8a850653d2090
lavish <lavish.pareta@enbraun.com>
2025-12-10 16:53:06 +0530

    Fix: Resource Update Form dynamic

--- nodes/ErsApp/resources/resource/update.ts
+++ nodes/ErsApp/resources/resource/update.ts
@@ -21,83 +21,260 @@ export const resourceUpdateDescription: INodeProperties[] = [
 		description: 'Unique ID of the resource to update',
 	},
 	{
-		displayName: 'First Name',
+		displayName: 'Resource Type Name or ID',
+		name: 'resource_type_id',
+		type: 'options',
+		displayOptions: {
+			show: showOnlyForResourceUpdate,
+		},
+		typeOptions: {
+			loadOptionsMethod: 'getResourceTypes',
+		},
+		default: '',
+		description: 'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>',
+		routing: {
+			send: {
+				property: 'resource_type_id',
+				type: 'body',
+				value: '={{ (() => { try { if (typeof $value === "string") { const parsed = JSON.parse($value); if (parsed && typeof parsed === "object" && "id" in parsed) { return parsed.id; } } } catch (e) { } return $value; })() }}',
+			},
+		},
+	},
+	{
+		displayName: 'Resource Name',
 		name: 'first_name',
 		type: 'string',
 		displayOptions: {
 			show: showOnlyForResourceUpdate,
 		},
 		default: '',
-		description: 'First name of the resource',
-		routing: {
-			send: {
-				property: 'first_name',
-				type: 'body',
-			},
-		},
+		description: 'Name of the resource (sent as "first_name" for human resources or "name" for non-human resources). Optional - only included in request if provided.',
 	},
 	{
-		displayName: 'Additional Fields',
-		name: 'additionalFields',
-		type: 'collection',
-		placeholder: 'Add Field',
+		displayName: 'User Defined Fields',
+		name: 'udfFields',
+		type: 'fixedCollection',
+		placeholder: 'Add UDF Field',
 		displayOptions: {
-			show: showOnlyForResourceUpdate,
+			show: {
+				...showOnlyForResourceUpdate,
+				resource_type_id: [
+					{
+						_cnd: {
+							regex: '.+', // Show when resource_type_id has any value
+						},
+					},
+				],
+			},
 		},
 		default: {},
+		typeOptions: {
+			multipleValues: true,
+		},
+		description: 'Custom user-defined fields from eResource Scheduler. Fields are fetched dynamically based on the selected Resource Type. After selecting a field, fill ONLY the appropriate value field that matches the field type (Text for TEXT/EMAIL/ENAME, Number for NUMBER/INTEGER, Date for DATE, Boolean for BOOLEAN/CHECKBOX, Select for dropdowns with options, Multi-Select for multi-select dropdowns).',
 		options: [
 			{
-				displayName: 'Calendar ID',
-				name: 'calendar',
-				type: 'number',
-				placeholder: '',
-				default: '',
-				description: 'ID of Calendar to assign to resource',
-				routing: {
-					send: {
-						property: 'calendar',
-						type: 'body',
+				displayName: 'Field',
+				name: 'field',
+				values: [
+					{
+						displayName: 'Field Name or ID',
+						name: 'fieldName',
+						type: 'options',
+						typeOptions: {
+							loadOptionsMethod: 'getResourceUDFFields',
+						},
+						default: '',
+						description: 'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>',
+						required: true,
 					},
-				},
-			},
-			{
-				displayName: 'Email',
-				name: 'email',
-				type: 'string',
-				placeholder: 'name@email.com',
-				default: '',
-				routing: {
-					send: {
-						property: 'email',
-						type: 'body',
+					{
+						displayName: 'Field Value (Boolean)',
+						name: 'fieldValueBoolean',
+						type: 'boolean',
+						default: false,
+						displayOptions: {
+							show: {
+								fieldName: [
+									{
+										_cnd: {
+											regex: '.*"field_type":"CHK".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"BOOLEAN".*',
+										},
+									},
+								],
+							},
+						},
+						description: 'Whether to fill this for BOOLEAN, CHECKBOX field types',
 					},
-				},
-			},
-			{
-				displayName: 'Last Name',
-				name: 'last_name',
-				type: 'string',
-				default: '',
-				description: 'Last name of the resource',
-				routing: {
-					send: {
-						property: 'last_name',
-						type: 'body',
+					{
+						displayName: 'Field Value (Date)',
+						name: 'fieldValueDate',
+						type: 'dateTime',
+						default: '',
+						displayOptions: {
+							show: {
+								fieldName: [
+									{
+										_cnd: {
+											regex: '.*"field_type":"DATE".*',
+										},
+									},
+								],
+							},
+						},
+						description: 'Fill this for DATE field types',
 					},
-				},
-			},
-			{
-				displayName: 'Phone Number',
-				name: 'phone',
-				type: 'string',
-				placeholder: '',
-				default: '',
-				routing: {
-					send: {
-						property: 'phone',
-						type: 'body',
+					{
+						displayName: 'Field Value (Multi-Select) Name or ID',
+						name: 'fieldValueMultiSelect',
+						type: 'options',
+						noDataExpression: true,
+						typeOptions: {
+							loadOptionsMethod: 'getResourceUDFFieldOptions',
+							multipleValues: true,
+						},
+						default: [],
+						displayOptions: {
+							show: {
+								fieldName: [
+									{
+										_cnd: {
+											regex: '.*"field_type":"DDMS".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"ROLES".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"CHGRP".*',
+										},
+									},
+								],
+							},
+						},
+						description: 'Select multiple options from the dropdown. Selected values will be sent as an array of IDs. Choose from the list, or specify IDs using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
 					},
-				},
+					{
+						displayName: 'Field Value (Number)',
+						name: 'fieldValueNumber',
+						type: 'number',
+						default: 0,
+						displayOptions: {
+							show: {
+								fieldName: [
+									{
+										_cnd: {
+											regex: '.*"field_type":"NUMBER".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"INTEGER".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"FLOAT".*',
+										},
+									},
+								],
+							},
+						},
+						description: 'Fill this for NUMBER, INTEGER, FLOAT field types',
+					},
+					{
+						displayName: 'Field Value (Select) Name or ID',
+						name: 'fieldValueSelect',
+						type: 'options',
+						noDataExpression: true,
+						typeOptions: {
+							loadOptionsMethod: 'getResourceUDFFieldOptions',
+						},
+						default: '',
+						displayOptions: {
+							show: {
+								fieldName: [
+									{
+										_cnd: {
+											regex: '.*"field_type":"RTYPE".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"DDSS".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"ROLESS".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"USS".*',
+										},
+									},
+								],
+							},
+							hide: {
+								fieldName: [
+									{
+										_cnd: {
+											regex: '.*"field_type":"DDMS".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"ROLES".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"CHGRP".*',
+										},
+									},
+								],
+							},
+						},
+						description: 'Fill this for single-select dropdown fields. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+					},
+					{
+						displayName: 'Field Value (Text)',
+						name: 'fieldValueText',
+						type: 'string',
+						default: '',
+						displayOptions: {
+							show: {
+								fieldName: [
+									{
+										_cnd: {
+											regex: '.*"field_type":"TEXT".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"EMAIL".*',
+										},
+									},
+									{
+										_cnd: {
+											regex: '.*"field_type":"ENAME".*',
+										},
+									},
+								],
+							},
+						},
+						description: 'Fill this for TEXT, EMAIL, ENAME field types',
+					},
+				],
 			},
 		],
 	},
