b7d87f7c37ed4d25dcb8f8556e242201ef919918
Lavish <lavish.pareta@enbraun.com>
2026-03-06 13:05:12 +0530

    #22 Force Delete Added for Project

--- nodes/ErsApp/resources/project/delete.ts
+++ nodes/ErsApp/resources/project/delete.ts
@@ -20,5 +20,39 @@ export const projectDeleteDescription: INodeProperties[] = [
 		default: '',
 		description: 'Unique ID of the project to delete',
 	},
+	{
+		displayName: 'Force Delete Options',
+		name: 'forceDeleteOptions',
+		type: 'collection',
+		placeholder: 'Add option',
+		default: {},
+		displayOptions: {
+			show: showOnlyForProjectDelete,
+		},
+		options: [
+			{
+				displayName: 'Force Delete Requirements',
+				name: 'force_delete_requirements',
+				type: 'boolean',
+				default: false,
+				description: 'Whether to delete the project along with all associated requirements',
+			},
+			{
+				displayName: 'Force Delete Bookings',
+				name: 'force_delete_bookings',
+				type: 'boolean',
+				default: false,
+				description: 'Whether to delete the project along with all associated bookings',
+			},
+			{
+				displayName: 'Force Delete Timesheet Entries',
+				name: 'force_delete_timesheet_entry',
+				type: 'boolean',
+				default: false,
+				description: 'Whether to delete the project along with all associated timesheet entries',
+			},
+		],
+		description: 'Optional flags to force delete related requirements, bookings, and timesheet entries',
+	},
 ];
 
